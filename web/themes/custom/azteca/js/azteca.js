a1e29c524027fa09959b8d3fa4baa475aafeb021
Dayscript SAS <soporte@dayscript.com>
2021-03-02 11:45:34 -0500

    update css

--- web/themes/custom/azteca/js/azteca.js
+++ web/themes/custom/azteca/js/azteca.js
@@ -11,7 +11,17 @@
   Drupal.behaviors.azteca = {
     attach: function (context, settings) {
       //alert("I'm alive!");
-      $('#block-secondarymenuhome .card-item > a').attr('target','_blank');
+      $('#block-secondarymenuhome .card-item > a, nav.main .cuenta a').attr('target','_blank');
+      $('#block-views-block-detalle-contenido-block-4').hide();
+      $('a.prices').click(function() {
+      	if ($('#block-views-block-detalle-contenido-block-4:visible').length == 1) {
+      		$('#block-views-block-detalle-contenido-block-4').hide();
+      		$('a.prices').text('Conoce los precios');
+      	}else{
+      		$('#block-views-block-detalle-contenido-block-4').show();
+      		$('a.prices').text('˄');
+      	}
+      });
     }
   };
 
